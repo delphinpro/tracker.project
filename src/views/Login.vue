bacf31ede545d78845eeecb97bcb5a4e0998aa1e
delphinpro <delphinpro@gmail.com>
2019-01-10 06:52:09 +0400

    add: route /login

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <form class="login">
+            <h1>Sign in</h1>
+
+            <label>User name</label>
+            <input required type="text" placeholder="Login"/>
+            <br>
+
+            <label>Password</label>
+            <input required type="password" placeholder="Password"/>
+
+            <hr/>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
