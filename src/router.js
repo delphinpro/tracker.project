7e643158baa002e8c1c1ba7e16a111879a329c17
delphinpro <delphinpro@gmail.com>
2019-01-10 06:47:22 +0400

    add: route 404

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import PageNotFound from './views/PageNotFound.vue';
 
 
 Vue.use(Router);
@@ -22,5 +23,6 @@ export default new Router({
             // which is lazy-loaded when the route is visited.
             component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
         },
+        { path: '*', component: PageNotFound },
     ],
 });
